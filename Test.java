79581e9cec6920e2a9e7d4530035ededaf976db8
g-shafiee <33800821+g-shafiee@users.noreply.github.com>
2017-11-19 15:11:40 +0330

    Update Test.java

--- Test.java
+++ Test.java
@@ -18,7 +18,7 @@ import java.io.IOException;
         static class MyHandler implements HttpHandler {
             @Override
             public void handle(HttpExchange t) throws IOException {
-                String response = "This is the response";
+                String response = "This is the changed response.";
                 t.sendResponseHeaders(200, response.length());
                 OutputStream os = t.getResponseBody();
                 os.write(response.getBytes());
